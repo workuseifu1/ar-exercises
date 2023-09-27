70f6725f9f4ca03c9ca5c06422696e11562fe2ed
workuseifu1 <workuseifu1@gmail.com>
2023-09-26 21:58:58 -0500

    exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,18 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+print "Enter the store name: "
+store_name = gets.chomp
+
+new_store = Store.new(name: store_name)
+if new_store.save
+  puts "Store successfully created!"
+else
+  puts "Failed to create the store. Please fix the following errors:"
+  new_store.errors.each do |error|
+    attribute = error.attribute
+    message = error.message
+    puts "#{attribute.capitalize}: #{message}"
+  end
+end
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,15 @@
 class Store < ActiveRecord::Base
   has_many :employees
+
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validate :must_carry_apparel
+
+  private
+
+  def must_carry_apparel
+    unless mens_apparel || womens_apparel
+      errors.add(:base, "Stores must carry at least one of men's or women's apparel")
+    end
+  end
 end
