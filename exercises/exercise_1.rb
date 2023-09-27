0ede4445525381e9d572ec121739d0006e5026a4
workuseifu1 <workuseifu1@gmail.com>
2023-09-26 20:46:02 -0500

    exercise1-complete

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -7,3 +7,6 @@ puts "----------"
 burnaby = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
 richmond = Store.create(name: "Richmond", annual_revenue: 1260000, womens_apparel: true)
 gastown = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true)
+
+store_count = Store.count
+puts "Number of stores: #{store_count}"
